4e23e69df77e4395ddfef19a2609aacbda6951d6
Robert Grigore <robert.grigore@vision.ro>
2025-07-22 15:25:45 +0300

    Upload

---
+++ Controllers/FoodController.cs
@@ -0,0 +1,48 @@
+﻿using Day1Speedrun.Models;
+using Day1Speedrun.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day1Speedrun.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly FoodService _foodService;
+
+        public FoodController(FoodService foodService)
+        {
+            _foodService = foodService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_foodService.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Food food)
+        {
+            _foodService.Add(food);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Food food)
+        {
+            _foodService.Update(id, food);
+
+            return Ok(id);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _foodService.DeleteById(id);
+
+            return Ok("Food was deleted successfully");
+        }
+    }
+}
---
+++ Services/FoodService.cs
@@ -0,0 +1,68 @@
+﻿using Day1Speedrun.Models;
+
+namespace Day1Speedrun.Services
+{
+
+    public class FoodService
+    {
+        private Random _random = new Random();
+
+        private List<Food> _context = new List<Food>()
+        {
+            new Food {Id = 1, Name = "Pasta Carbonara", Ingredients = new List<string> { "Pasta", "Guanciale", "Egg", "Whatever" }, IsVegan = false},
+            new Food {Id = 2, Name = "Egg Toast", Ingredients = new List<string> { "Egg", "Toast"}, IsVegan = false },
+            new Food {Id = 3, Name = "Green Salad", Ingredients = new List<string> { "Lettuce", "Tomatoes", "Olives"}, IsVegan = true }
+        };
+
+        public FoodService() { }
+
+        public List<Food> GetAll()
+        {
+            return _context;
+        }
+
+        public Food? GetById(int id)
+        {
+            return _context.FirstOrDefault(f => f.Id == id);
+        }
+
+        public int Add(Food food)
+        {
+            _context.Add(food);
+
+            return food.Id;
+        }
+
+        public void DeleteById(int id)
+        {
+            var foodToDelete = _context.FirstOrDefault(f => f.Id == id);
+
+            if (foodToDelete is null)
+                return;
+
+            _context.Remove(foodToDelete);
+        }
+
+        public Food? FindByName(string name)
+        {
+            return _context.FirstOrDefault(f => f.Name == name);
+        }
+
+        public Food Update(int id, Food food)
+        {
+            int indexToModify = _context.FindIndex(f => f.Id == id);
+
+            _context[indexToModify] = food;
+
+            return food;
+        }
+
+        public Food? GetRandom()
+        {
+            int randomElementId = _random.Next(1, _context.Count);
+
+            return _context.FirstOrDefault(f => f.Id == randomElementId);
+        }
+    }
+
+}
